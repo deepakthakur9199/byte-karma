00d7b1530cc2f748c9117dec38637632ec913267
deepakthakur9199 <imdeepak752@gmail.com>
2024-03-04 01:47:51 -0800

    add initial file

---
+++ script.js
@@ -0,0 +1,10 @@
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var destination = document.getElementById("destination").value;
+    var timing = document.getElementById("timing").value;
+    
+    // Do something with the form data (e.g., send it to a server)
+    console.log("Name:", name);
+    console.log("Destination:", destination);
+    console.log("Timing:", timing);
+  }
